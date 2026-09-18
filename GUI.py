bb6277ed917b603c40e77fc45a4221a4189d4f7e
Suf-jou <maskedgamer8@outlook.com>
2026-09-18 16:40:45 +0100

    GUI

---
+++ GUI.py
@@ -0,0 +1,31 @@
+import tkinter as tk
+
+from core import autoclicker
+
+root = tk.Tk()
+root.geometry("800x600")
+root.title("Auto Clicker")
+
+def toggle_clicking():
+    if button["text"] == "Start Clicking":
+        button["text"] = "Stop Clicking"
+
+
+    else:
+        button["text"] = "Start Clicking"
+
+label = tk.Label(root, text="Auto Clicker", font=("Arial", 24))
+label.pack(pady=20)
+
+
+
+button = tk.Button(root, text="Start Clicking", font=("Arial", 16), command=toggle_clicking)
+button.pack(pady=20)
+
+
+
+scalar = tk.Scale(root, from_=0.1, to=5.0, resolution=0.1, orient=tk.HORIZONTAL, label="Click Interval (seconds)", font=("Arial", 10), length=300) 
+scalar.pack(pady=20)
+
+
+root.mainloop()
