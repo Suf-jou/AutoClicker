32b4c12f7632808455f4a4bca95b9ddfc4e8cbd5
Suf-jou <maskedgamer8@outlook.com>
2026-09-18 15:07:36 +0100

    functions

--- core.py
+++ core.py
@@ -1,6 +1,24 @@
+import pyautogui
+from time import sleep
 class autoclicker():
     def __init__(self):
-        self.running = True
+        self.running = False
         self.interval = 0.1
 
-    
+    def click(self):
+        pyautogui.click()
+
+    def stop(self):
+        self.running = False
+
+    def loop(self):
+        while self.running:
+            self.click()
+            sleep(self.interval)
+
+    def set_interval(self, interval):
+        self.interval = interval
+
+    def start(self):
+        self.running = True
+        self.loop()
